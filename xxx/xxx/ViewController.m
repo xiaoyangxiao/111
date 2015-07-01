7ceed6621ad976b484d3ffcc2224566076d11851
Mustc <tarena11@dayenidejiangchoule.local>
2015-07-01 11:49:44 +0800

    qq

---
+++ xxx/xxx/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  xxx
+//
+//  Created by Mustc on 15/7/1.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"小冲冲");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
